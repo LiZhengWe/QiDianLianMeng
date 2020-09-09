d07ec74d3681fce0e17e62f6dd1a37893b68510f
yiner <yiner@2dfire.com>
2020-09-09 15:38:09 +0800

    资源文件添加

---
+++ QiDian/Home（首页tab）/View/QDHomeYeJiTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  QDHomeYeJiTableViewCell.m
+//  QiDian
+//
+//  Created by yiner on 2020/9/9.
+//  Copyright © 2020 Henan Sanliuba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import "QDHomeYeJiTableViewCell.h"
+
+@implementation QDHomeYeJiTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
